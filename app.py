eb4597225e77ebd58147fc9108365bdf54f009f4
shashank317 <s2770184@gmail.com>
2025-07-29 22:33:38 +0530

    changed

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 import numpy as np
-from flask  import Flask, request, render_template
+from flask import Flask, request, render_template
 import pickle
 
-flask_app = Flask(__name__)
-model =pickle.load(open('model.pkl', "rb"))
+app = Flask(__name__)  # ← renamed here
+model = pickle.load(open('model.pkl', "rb"))
 
-@flask_app.route('/')
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@flask_app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     float_feature = [float(x) for x in request.form.values()]
     features = [np.array(float_feature)]
     prediction = model.predict(features)
-    return render_template('index.html', prediction_text='Predicted Crop is{}'.format(prediction))
+    return render_template('index.html', prediction_text='Predicted Crop is {}'.format(prediction))
 
 if __name__ == "__main__":
-    flask_app.run(debug=True)
-
+    app.run(debug=True)
